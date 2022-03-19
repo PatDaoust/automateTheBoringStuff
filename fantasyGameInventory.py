da9a57e9867cf434d1d06dffb691ea3d8f44fc31
Cataluna <catalunalilith2680@gmail.com>
2022-03-19 17:43:34 -0400

    implement addToInventory() without modifying the original dictionary

--- fantasyGameInventory.py
+++ fantasyGameInventory.py
@@ -46,4 +46,54 @@ def displayInventory(inventory_dict):
 
 
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
-displayInventory(stuff)
+# displayInventory(stuff)
+
+
+"""
+Imagine that a vanquished dragon’s loot is represented as a list of strings like this:
+
+
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+Write a function named addToInventory(inventory, addedItems),
+where the inventory parameter is a dictionary representing the player’s inventory
+(like in the previous project) and the addedItems parameter is a list like dragonLoot.
+The addToInventory() function returns a dictionary that represents the updated inventory.
+Note that the addedItems list can contain multiples of the same item.
+Your code could look something like this:
+
+
+def addToInventory(inventory, addedItems):
+    # your code goes here
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
+The previous program (displayInventory()) would output the following:
+
+
+Inventory:
+45 gold coin
+1 rope
+1 ruby
+1 dagger
+
+Total number of items: 48
+"""
+
+
+def addToInventory(inventory_dict, loot_list):
+    """assumes inventory_dict s a dictonary with string keys and numeric values
+    assumes loot_list is a list of strings
+    returns a dict, an updated inventory including the loot
+    warning: strings from loot_list and inventory_dict must match exactly"""
+    new_inventory = dict(inventory_dict)
+    for item in loot_list:
+        new_inventory.setdefault(item, 0)
+        new_inventory[item] += 1
+    return new_inventory
+
+
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+updated_inventory = addToInventory(stuff, dragon_loot)
+displayInventory(updated_inventory)
