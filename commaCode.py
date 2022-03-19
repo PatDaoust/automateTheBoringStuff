f7b6ad7b8aaf1fc1e3304d15fd89c180aa8fa1aa
Cataluna <catalunalilith2680@gmail.com>
2022-03-19 14:58:54 -0400

    plan for commaCode()

---
+++ commaCode.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 19 14:55:43 2022
+
+@author: catal
+"""
+
+"""Say you have a list value like this:
+
+
+spam = ['apples', 'bananas', 'tofu', 'cats']
+Write a function that takes a list value as an argument and
+returns a string with all the items separated by a comma and a space,
+with and inserted before the last item.
+For example, passing the previous spam list to the function would return
+'apples, bananas, tofu, and cats'.
+But your function should be able to work with any list value passed to it.
+"""
+
+
+def commaCode(a_list):
+    """assumes a_list is a list of strings
+    returns a string, the contents of a_list with ", " between items"""
+    pass
+
+
+spam = ['apples', 'bananas', 'tofu', 'cats']
+commaCode(spam)
