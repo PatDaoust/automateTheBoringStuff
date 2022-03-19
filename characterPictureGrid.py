35b776a2e1c5de5cb57815689983eb3970727dbd
Cataluna <catalunalilith2680@gmail.com>
2022-03-19 15:21:06 -0400

    implement characterPictureGrid()

--- characterPictureGrid.py
+++ characterPictureGrid.py
@@ -27,27 +27,33 @@ and the y-coordinates increase going down.
 
 Copy the previous grid value, and write code that uses it to print the image.
 
-
 ..OO.OO..
 .OOOOOOO.
 .OOOOOOO.
 ..OOOOO..
 ...OOO...
 ....O....
+
 Hint: You will need to use a loop in a loop in order to print grid[0][0], then grid[1][0],
 then grid[2][0], and so on, up to grid[8][0].
 This will finish the first row, so then print a newline.
 Then your program should print grid[0][1], then grid[1][1], then grid[2][1], and so on.
 The last thing your program will print is grid[8][5].
 
-Also, remember to pass the end keyword argument to print() if you don’t want a newline printed automatically after each print() call.
+Also, remember to pass the end keyword argument to print()
+if you don’t want a newline printed automatically after each print() call.
 """
 
 
 def characterPictureGrid(a_grid):
     """assumes a_grid is a list of lists of strings of length 1
     prints the message"""
-    pass
+    row_num = len(grid)
+    column_num = len(grid[0])
+    for i in range(column_num):
+        for j in range(row_num):
+            print(a_grid[j][i], end="")
+        print("\n")
 
 
 grid = [['.', '.', '.', '.', '.', '.'],
