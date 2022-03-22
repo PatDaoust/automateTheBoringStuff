7744617b6df9db9eb05f8d3ea9f36eaca737c66c
Cataluna <catalunalilith2680@gmail.com>
2022-03-22 19:23:46 -0400

    implement code step 1

--- passwordLocker.py
+++ passwordLocker.py
@@ -6,3 +6,18 @@ Created on Tue Mar 22 19:18:44 2022
 """
 
 # pw.py - An insecure password locker program
+
+"""
+goal = create basic password locker
+input = string, account name
+putput = string to clipboard
+
+structure = dictionary
+key = string, account name
+value = string, password
+
+"""
+
+password_dict = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+                 'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+                 'luggage': '12345'}
