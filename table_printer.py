49a1e33da7796c30e8efa096cfd3a48747bdd7d5
Cataluna <catalunalilith2680@gmail.com>
2022-03-23 16:16:55 -0400

    message

---
+++ table_printer.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 23 16:12:32 2022
+
+@author: catal
+"""
+"""
+Write a function named printTable() that takes a list of lists of strings
+and displays it in a well-organized table with each column right-justified.
+Assume that all the inner lists will contain the same number of strings.
+"""
+
+table_data = [['apples', 'oranges', 'cherries', 'banana'],
+              ['Alice', 'Bob', 'Carol', 'David'],
+              ['dogs', 'cats', 'moose', 'goose']]
+
+
+def tablePrinter(list_list):
+    """assumes list_list is a list of lists of strings,
+    where the inner lists are all of the same lenght
+    prints list_list in table for
+    returns None"""
+    pass
+
+
+if __name__ == "__main__":
+    tablePrinter(table_data)
