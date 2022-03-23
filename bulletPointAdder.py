182c3978dcc077a248cf12968e0f29979af1413b
Cataluna <catalunalilith2680@gmail.com>
2022-03-23 12:12:05 -0400

    innitial commit of bulletPointAdder

---
+++ bulletPointAdder.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 23 12:09:27 2022
+
+@author: catal
+"""
+import pyperclip
+
+
+def bulletPointAdder():
+    """gets the text from the clipboard
+    adds a star and space to the beginning of each line
+    pastes this new text to the clipboard
+    returns None
+    """
+    pass
+
+
+if __name__ == "__main__":
+    bulletPointAdder()
