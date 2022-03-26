3a0ca7feeb063f4a45751ace22340da807c5aaf4
Cataluna <catalunalilith2680@gmail.com>
2022-03-26 12:11:09 -0400

    initial commit of extractPhoneEmail plan

---
+++ extractPhoneEmail.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 26 11:57:07 2022
+
+@author: catal
+"""
+
+"""
+Get the text off the clipboard.
+pyperclip.paste()
+
+Find all phone numbers and email addresses in the text.
+regex for phone #s
+regex for emails
+findall() returns a list of string matches
+combine phone #s and emails into string
+\n seperated
+
+Paste them onto the clipboard.
+pyperclip.copy()
+"""
