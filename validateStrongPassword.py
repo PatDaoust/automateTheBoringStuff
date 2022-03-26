2ce2da0e8f526da2c698a3b676f05e5196bb96db
Cataluna <catalunalilith2680@gmail.com>
2022-03-26 16:27:03 -0400

    implement validateStrongPassword

--- validateStrongPassword.py
+++ validateStrongPassword.py
@@ -4,7 +4,7 @@ Created on Sat Mar 26 16:03:06 2022
 
 @author: catal
 """
-
+import re
 """
 Strong Password Detection
 Write a function that uses regular expressions to make sure the password string
@@ -20,9 +20,28 @@ def validateStrongPassword(password):
     """assumes password is a string
     returns a boolean, True is password is judged to be sufficiently strong,
     else False"""
-    pass
+    # is 8+ chars
+    re_8chars = re.compile("(.){8,}")
+    if not re.search(re_8chars, password):
+        return False
+    # uppercase and lowercase
+    re_upper = re.compile("[A-Z]")
+    if not re.search(re_upper, password):
+        return False
+    re_lower = re.compile("[a-z]")
+    if not re.search(re_lower, password):
+        return False
+    # digit
+    re_digit = re.compile("\d")
+    if not re.search(re_digit, password):
+        return False
+    return True
 
 
 if __name__ == "__main__":
-    validateStrongPassword("CurryIs#1Cat")
-    validateStrongPassword("Curry")
+    pass
+    # print(validateStrongPassword("CurryIs#1Cat"))
+    # print(validateStrongPassword("Cu1"))
+    # print(validateStrongPassword("curryis#1cat"))
+    # print(validateStrongPassword("CURRYIS#1CAR"))
+    # print(validateStrongPassword("CurryIsCat"))
